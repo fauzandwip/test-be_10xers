5a0c448795b5603b10aa55f7706516293f5e173a
fauzandwip <fauzan27dp@gmail.com>
2024-04-30 19:13:10 +0700

    feat: create route and controller for user login feat: create and implement errorHandler middleware feat: create jsonwebtoken helper

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// all router
+app.use(require('./routes'));
+
 app.get('/test', (req, res) => res.send('Test 10xers'));
+
+// error handler middleware
+app.use(errorHandler);
 
 module.exports = app;
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,40 @@
+const errorHandler = (err, req, res, next) => {
+	let message, messages, statusCode;
+
+	switch (err.name) {
+		case 'SequelizeValidationError':
+			statusCode = 400;
+			messages = err.errors.map((error) => {
+				return error.message;
+			});
+			break;
+
+		case 'BadRequest':
+			statusCode = 400;
+			message = err.message ?? 'Invalid user input';
+			break;
+
+		case 'Unauthenticated':
+			statusCode = 401;
+			message = err.message ?? 'Unauthenticated';
+			break;
+
+		case 'SequelizeConstraintError':
+			statusCode = 409;
+			message = err.errors[0].message;
+			break;
+
+		default:
+			statusCode = 500;
+			message = 'Internal Server Error';
+			break;
+	}
+
+	console.log(err, statusCode);
+	res.status(statusCode).json({
+		status: 'error',
+		[message ? 'message' : 'messages']: message ? message : messages,
+	});
+};
+
+module.exports = errorHandler;
